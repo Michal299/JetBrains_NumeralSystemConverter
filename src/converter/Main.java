5a503a40fb3b5eddebd9b23fa779398392ba6a3a
Michał Błajet <michula299@gmail.com>
2020-08-25 14:12:24 +0200

    Integer and floating point numbers support

--- src/converter/Main.java
+++ src/converter/Main.java
@@ -1,8 +1,28 @@
 package converter;
 
-public class Main {
+import java.util.Scanner;
 
+public class Main {
     public static void main(String[] args) {
-	// write your code here
+        Scanner scanner = new Scanner(System.in);
+        int sourceRadix = scanner.nextInt();
+        String number = scanner.next();
+        int targetRadix = scanner.nextInt();
+
+        Converter converter = new Converter();
+
+        String targetRepresentation;
+        if (number.contains(".")) {
+            double decimalRepresentation = converter.convertToDoubleDecimal(number, sourceRadix);
+            targetRepresentation = converter.convertDecimalTo(decimalRepresentation, targetRadix);
+        } else {
+            long decimalRepresentation = converter.convertToLongDecimal(number, sourceRadix);
+            targetRepresentation = converter.convertDecimalTo(decimalRepresentation, targetRadix);
+        }
+
+
+        System.out.println(targetRepresentation);
+
+
     }
 }
